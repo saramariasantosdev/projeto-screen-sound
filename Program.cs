919be87103929bbafe669eb4cd4987d604d8e94e
Sara Maria <saramariasantosdev@gmail.com>
2025-07-27 14:13:45 -0300

    Cria menu usando condicional

--- Program.cs
+++ Program.cs
@@ -13,5 +13,26 @@ void ExibirMensagem()
     string mensagem = "Boas Vindas ao Screen Sound";
     Console.WriteLine(mensagem);
 }
+void ExibirMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nEscolha uma opção: ");
+    string opcao = Console.ReadLine()!;
+
+    if (int.Parse(opcao) == 1)
+    {
+        Console.WriteLine($"Você digitou a opção: {opcao}");
+    }
+    else if (int.Parse(opcao) == 2)
+    {
+        Console.WriteLine($"Você digitou a opção: {opcao}");
+    }
+}
 
 ExibirMensagem();
+ExibirMenu();
